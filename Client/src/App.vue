32df308e61fc03ec90bc366863af0248b00dab3d
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2024-01-25 23:07:29 +0100

    Banana

---
+++ Client/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-app-bar app color="purple" dark>
+      <div class="d-flex align-center">
+        Gamers Challenge
+      </div>
+      <v-spacer></v-spacer>
+      <v-btn text to="/">HOME</v-btn>
+      <v-btn v-show="!isAuthenticated" text :to="{ name: 'login' }" class="colored-button">Login</v-btn>
+      <v-btn v-show="!isAuthenticated" text :to="{ name: 'register' }" class="colored-button">Register</v-btn>
+      <v-btn v-show="isAuthenticated" @click="signOut" text>Sign Out</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <div v-if="selectedSuggestion">
+        <router-view :selectedSuggestion="selectedSuggestion" />
+      </div>
+      <router-view v-else />
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+import Homepage from '@/views/HomeView.vue';
+import Register from '@/views/register.vue';
+import Login from '@/views/login.vue';
+
+export default {
+  components: {
+    Homepage,
+    Register,
+    Login
+  },
+  data() {
+    return {
+      isAuthenticated: false,
+      selectedSuggestion: null,
+    };
+  },
+  methods: {
+    signOut() {
+      // Implementacija odjave
+    },
+  },
+};
+</script>
+
+<style scoped>
+body {
+  margin: 0;
+}
+</style>
---
+++ Client/src/views/register.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="register-container">
+    <h2>Register</h2>
+    <form @submit.prevent="submitForm">
+      <div class="input-field">
+        <label for="name">Ime</label>
+        <input type="text" id="name" v-model="name" />
+      </div>
+      <div class="input-field">
+        <label for="surname">Prezime</label>
+        <input type="text" id="surname" v-model="surname" />
+      </div>
+      <div class="input-field">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="email" />
+      </div>
+      <div class="input-field">
+        <label for="username">Korisničko ime</label>
+        <input type="text" id="username" v-model="username" />
+      </div>
+      <div class="input-field">
+        <label for="password">Lozinka</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button type="submit">Registriraj se</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      surname: '',
+      email: '',
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    submitForm() {
+      // Implementirajte logiku za slanje forme
+
+      // Nakon uspješnog slanja forme, navigirajte na početnu stranicu
+      this.$router.push({ name: 'home' }); // 'home' je ime rute definirano u Vue Routeru
+      this.$router.push({ name: 'login' });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.register-container {
+  background-color: purple;
+  padding: 20px;
+  color: white;
+}
+
+h2 {
+  text-align: left;
+}
+
+.input-field {
+  margin-bottom: 10px;
+}
+
+.input-field input {
+  background-color: white;
+  margin-top: 5px;
+  padding: 5px;
+}
+</style>
