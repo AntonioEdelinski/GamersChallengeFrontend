af1dd1c1c4e81719eacfec5539db9e3569d8211b
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2024-01-26 13:04:51 +0100

    git commit

--- Client/vue.config.js
+++ Client/vue.config.js
@@ -1,6 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
-})
+  ],
+  chainWebpack: (config) => {
+    config.plugin('extract-css').use(MiniCssExtractPlugin, [{
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].css',
+    }]);
+  },
+});
